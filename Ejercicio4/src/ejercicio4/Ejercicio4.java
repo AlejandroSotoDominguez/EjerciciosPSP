ba746d94cebf627120288da4e59e04b8d79a4126
Alex <Alex@Alex-PC>
2018-12-13 02:08:35 +0100

    Comentarios añadidos

--- Ejercicio4/src/ejercicio4/Ejercicio4.java
+++ Ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -7,12 +7,13 @@ public class Ejercicio4 extends Thread {
         super(nombre);
     }
 
+    // Hilo que suma los números pares de 1 a 1000
     static class Par extends Thread{
         public void run(){
         int contador = 0;
         int contadorPar = 0;
         int contadorImpar = 0;
-
+            // Recorremos el bucle y vamos acumulando la suma
             for(int i=1; i<=1000; i++){
                 if(i%2 == 0){
                     contadorPar += i;
@@ -24,7 +25,8 @@ public class Ejercicio4 extends Thread {
             System.out.println(contadorPar);     
         }
     }
-     
+    
+    // Hilo que suma los números impares
     static class Impar extends Thread{
         public void run(){
         int contadorImpar = 0;
@@ -40,7 +42,7 @@ public class Ejercicio4 extends Thread {
         }
     }  
     
-    
+    // Hilo que suma los números que terminan en 2 y en 3
     static class Terminacion extends Thread{
         int contadorTer=0;
         
@@ -55,6 +57,7 @@ public class Ejercicio4 extends Thread {
         }
     }
     
+    // Ejecutamos 3 hilos que se ejecutaran al mismo tiempo
     public static void main(String[] args) {
         new Par().start();
         new Impar().start();
